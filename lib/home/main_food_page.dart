af0f2b1c4b80227b10592afbd878f54574797ddc
Nethum Pabasara <lnpariyarathna@students.nsbm.ac.lk>
2023-04-07 08:20:10 +0530

    main food page created.

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({super.key});
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

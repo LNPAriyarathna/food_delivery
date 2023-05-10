8ebcb87fb9d82c18d950300c474dc74591dc5db9
Nethum Pabasara <lnpariyarathna@students.nsbm.ac.lk>
2023-05-10 13:15:55 +0530

    update_the_widgets

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,5 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/colors.dart';
+import 'package:food_delivery/widgets/small_text.dart';
+
+import 'food_page_body.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({super.key});
@@ -11,6 +18,51 @@ class MainFoodPage extends StatefulWidget {
 class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(top: 45, bottom: 15),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(text: "Bangladesh", color: AppColors.mainColor),
+                      Row(
+                        children: [
+                          SmallText(
+                            text: "city",
+                            color: Colors.black54,
+                          ),
+                          Icon(Icons.arrow_drop_down_rounded)
+                        ],
+                      )
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                      width: 45,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          FoodPageBody(),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widgets/big_text.dart
@@ -0,0 +1,27 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BigText extends StatelessWidget {
+  final Color? color;
+  final String text;
+  double size;
+  TextOverflow overflow;
+  BigText(
+      {super.key,
+      this.color = const Color(0xFF332d2b),
+      required this.text,
+      this.size = 20,
+      this.overflow = TextOverflow.ellipsis});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text,
+        maxLines: 1,
+        overflow: overflow,
+        style: TextStyle(
+            color: color, fontSize: size, fontWeight: FontWeight.w400));
+  }
+}
